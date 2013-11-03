a520c47c0c6da34e0e81a826bff20dd821e980ef
Sagar Ahire <djsagarahire@gmail.com>
2013-11-03 14:50:57 +0530

    Merged dependency parse with similarity; made code more pythonic

--- GetDependencyParse.py
+++ GetDependencyParse.py
@@ -1,19 +1,29 @@
-import os
-import re
-import nltk
-sentence = "children are roses."
+""" GetDependencyParse.py - Contains functions to deal with output of Stanford's dependency parser
+"""
 
-#text=nltk.word_tokenize(sentence)
-#tagged = nltk.pos_tag(text)
-print tagged
-os.popen("echo '"+sentence+"' > ~/Documents/stanfordtemp.txt")
-parser_out = os.popen("~/softwares/stanford_parser/lexparser.sh ~/Documents/stanfordtemp.txt").readlines()
-#print parser_out
-print "---"
-dependency_parse=[]
-for i in parser_out:
-	 if len(i.strip()) > 0 and i.strip()[0] != "(":
-		line=i.strip()
-		dependency_parse.append(filter(lambda x:x.isalpha(),re.findall(r"[\w']+", line)))
-		
-print dependency_parse
+import re
+from parse import parse
+
+def dependency_parse(sentence):
+    """ Accepts a sentence and returns its dependency parse as a list-of-lists
+    """
+
+    # sentence = raw_input("Enter a sentence: ")
+
+    parser_folder = "parser"    # Change if parser is in some other directory
+    parse_output = parse(sentence, parser_folder)
+    dep_parse = parse_output[1].split("\n")
+
+    print "---"
+    dependency_parse=[]
+    for i in dep_parse:
+        if len(i.strip()) > 0 and i.strip()[0] != "(":
+            line=i.strip()
+            dependency_parse.append(filter(lambda x:x.isalpha(),re.findall(r"[\w']+", line)))
+
+    return dependency_parse
+
+if __name__ == "__main__":
+    sentence = raw_input("Enter a sentence: ")
+
+    print dependency_parse(sentence)
--- Similarity.py
+++ Similarity.py
@@ -1,24 +1,63 @@
 from nltk.corpus import wordnet as wn
-word1 = wn.synsets('children')
-word2 = wn.synsets('babies')
-max_sim = 0
-max_sim_w1=""
-max_sim_w2=""
-for w1 in word1:
-	for w2 in word2:
-		sim = w1.path_similarity(w2)
-		#sim = w1.lch_similarity(w2)
-		#sim = w1.wup_similarity(w2)
-		#sim = w1.res_similarity(w2,corpus)
-		#sim = w1.jcn_similarity(w2,corpus)
-		#sim = w1.lin_similarity(w2,corpus)
-		
-		if max_sim < sim:
-			max_sim = sim
-			max_sim_w1=w1
-			max_sim_w2=w2
+from GetDependencyParse import dependency_parse
 
-print max_sim
-print max_sim_w1
-print max_sim_w2
+def detect_metaphor(sentence):
+    """ Accepts a sentence and outputs similarity of noun-noun pairs related by nsubj
+    """
+
+    dep_parse_output = dependency_parse(sentence)
+
+    nsubj_pairs = [ns_parse[1:] for ns_parse in filter(lambda dp: dp[0] == "nsubj", dep_parse_output)]
+
+    for pair in nsubj_pairs:
+        print "\nInvestigating metaphor for pair {0}".format(pair)
+
+        syn_pair = []
+
+        for word in pair:
+
+            synsets = wn.synsets(word)
+            print "What sense of '{0}' would you like to use?".format(word)
+            for i, synset in enumerate(synsets):
+                print "{0}: {1}".format(i, synset.definition)
+            chosen_id = int(raw_input("Enter number corresponding to sense: "))
+
+            syn_pair.append(synsets[chosen_id])
+
+        similarity_measure = similarity(syn_pair[0], syn_pair[1])
+        print "Similarity is {0}".format(similarity_measure)
+
+def similarity(synset1, synset2):
+    """ Accepts 2 synsets and returns the similarity
+    """
+
+    return synset1.path_similarity(synset2)
+
+if __name__ == "__main__":
+    sentence = raw_input("Enter a sentence: ")
+
+    detect_metaphor(sentence)
+
+# word1 = wn.synsets('children')
+# word2 = wn.synsets('babies')
+# max_sim = 0
+# max_sim_w1=""
+# max_sim_w2=""
+# for w1 in word1:
+#     for w2 in word2:
+#         sim = w1.path_similarity(w2)
+# 	#sim = w1.lch_similarity(w2)
+# 	#sim = w1.wup_similarity(w2)
+# 	#sim = w1.res_similarity(w2,corpus)
+# 	#sim = w1.jcn_similarity(w2,corpus)
+# 	#sim = w1.lin_similarity(w2,corpus)
+		
+# 	if max_sim < sim:
+#             max_sim = sim
+# 	    max_sim_w1=w1
+# 	    max_sim_w2=w2
+
+# print max_sim
+# print max_sim_w1
+# print max_sim_w2
 
